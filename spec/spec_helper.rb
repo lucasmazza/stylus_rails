03b7de8f0052db821a934de0ef781f8f09dc857b
Lucas Mazza <luc4smazza@gmail.com>
2011-02-01 21:50:38 -0200

    remove fakefs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
-# require 'fakefs/spec_helpers'
 require 'stylus_rails'
 
 RSpec.configure do |config|
-  # config.include FakeFS::SpecHelpers
 
   def fixtures(*paths)
     paths.map { |path| File.expand_path(path, "spec/fixtures") }
