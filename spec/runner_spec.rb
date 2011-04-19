dd238c53352fbabe63f09fa442401a7ad8a580d4
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2011-04-19 03:59:31 +0200

    Specs are now expecting backticks instead of system call

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -34,51 +34,51 @@ describe Stylus::Runner do
     end
 
     it "calls the stylus cli" do
-      subject.should_receive(:system).with("stylus #{stylus_file} -o #{target_folder}")
+      subject.should_receive(:`).with("stylus #{stylus_file} -o #{target_folder} 2>&1")
       subject.call
     end
 
     it "runs files on the same folder on a single command" do
       paths = fixtures("stylus/simple.styl", "stylus/foo.styl")
       subject = Stylus::Runner.new(paths)
-      subject.should_receive(:system).with("stylus #{paths.join(" ")} -o #{target_folder}")
+      subject.should_receive(:`).with("stylus #{paths.join(" ")} -o #{target_folder} 2>&1")
       subject.call
     end
-
+  
     it "uses the compress flag when configured to" do
       Stylus.compress = true
-      subject.should_receive(:system).with("stylus #{stylus_file} -o #{target_folder} -c")
+      subject.should_receive(:`).with("stylus #{stylus_file} -o #{target_folder} -c 2>&1")
       subject.call
     end
   end
-
+  
   describe "#call with different compile_dir" do
     subject { Stylus::Runner.new(fixture("stylus/simple.styl")) }
     let(:stylus_file) { fixture("stylus/simple.styl").first }
     let(:target_folder) { File.dirname(fixture("output/simple.css").first) }
-
+  
     before do
       Stylus.compress = false
       Stylus.root = File.join(File.dirname(__FILE__), 'fixtures')
       Stylus.directory = 'stylus'
       Stylus.compile_directory = File.join(File.dirname(__FILE__), 'fixtures', 'output')
     end
-
+  
     it "calls the stylus cli" do
-      subject.should_receive(:system).with("stylus #{stylus_file} -o #{target_folder}")
+      subject.should_receive(:`).with("stylus #{stylus_file} -o #{target_folder} 2>&1")
       subject.call
     end
-
+  
     it "runs files on the same folder on a single command" do
       paths = fixtures("stylus/simple.styl", "stylus/foo.styl")
       subject = Stylus::Runner.new(paths)
-      subject.should_receive(:system).with("stylus #{paths.join(" ")} -o #{target_folder}")
+      subject.should_receive(:`).with("stylus #{paths.join(" ")} -o #{target_folder} 2>&1")
       subject.call
     end
-
+  
     it "uses the compress flag when configured to" do
       Stylus.compress = true
-      subject.should_receive(:system).with("stylus #{stylus_file} -o #{target_folder} -c")
+      subject.should_receive(:`).with("stylus #{stylus_file} -o #{target_folder} -c 2>&1")
       subject.call
     end
   end
