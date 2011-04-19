ebc4fcc7ad3858b4a0543178ac15b8a75515f947
Lucas Mazza <luc4smazza@gmail.com>
2011-04-18 23:45:18 -0300

    Silencing the puts call from the Runner class (in the future it should be replaced by a Logger instance)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,13 @@ require 'stylus_rails'
 
 RSpec.configure do |config|
 
+  config.before(:suite) { $stdout = StringIO.new }
+  config.after(:suite)  { $stdout = STDOUT }
+
   def fixtures(*paths)
     paths.map { |path| File.expand_path(path, "spec/fixtures") }
   end
+
   alias :fixture :fixtures
 end
 
